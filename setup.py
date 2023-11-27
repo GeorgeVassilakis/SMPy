8f60b92af13fd17f261ea85e7bc68b5de8d0e8ba
GeorgeVassilakis <vassilakis.g@northeastern.edu>
2023-11-27 09:41:47 -0500

    Add directory structure, setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SMPy',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy'
+    ],
+    author='Georgios N. Vassilakis',
+    author_email='vassilakis.g@northeastern.edu',
+    description='From Shear to Map: A Python-based approach to constructing convergence maps.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/GeorgeVassilakis/SMPy.git',
+)
