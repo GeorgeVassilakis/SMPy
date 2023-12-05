1eb7a5fbd27f77d28a663585154cbfd57f80df95
George Vassilakis <90641828+GeorgeVassilakis@users.noreply.github.com>
2023-12-05 13:12:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'numpy', 'astropy', 'pandas', 'matplotlib'
+        'numpy', 'astropy', 'pandas', 'matplotlib', 'scipy'
     ],
     author='Georgios N. Vassilakis',
     author_email='vassilakis.g@northeastern.edu',
-    description='From Shear to Map: A Python-based approach to constructing convergence maps.',
+    description='A Python-based approach to constructing convergence maps.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/GeorgeVassilakis/SMPy.git',
