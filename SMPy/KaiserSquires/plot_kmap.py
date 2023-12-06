88768b3ce5e292abe333dc90efd74931dde9f372
GeorgeVassilakis <vassilakis.g@northeastern.edu>
2023-12-05 19:43:16 -0500

    clean up plotting code

--- SMPy/KaiserSquires/plot_kmap.py
+++ SMPy/KaiserSquires/plot_kmap.py
@@ -7,10 +7,14 @@ def plot_convergence(convergence, boundaries, config):
 
     # Plot the convergence map
     plt.figure(figsize=config['figsize'])
-    plt.imshow(filtered_convergence[:, ::-1], cmap=config['cmap'],
-               vmax=config['vmax'], vmin=config['vmin'],
-               extent=[boundaries['ra_min'], boundaries['ra_max'], 
-                       boundaries['dec_min'], boundaries['dec_max']])
+    plt.imshow(filtered_convergence[:, ::-1], 
+               cmap=config['cmap'],
+               vmax=config['vmax'], 
+               vmin=config['vmin'],
+               extent=[boundaries['ra_min'], 
+                       boundaries['ra_max'], 
+                       boundaries['dec_min'], 
+                       boundaries['dec_max']])
     plt.colorbar()
     plt.xlabel(config['xlabel'])
     plt.ylabel(config['ylabel'])
