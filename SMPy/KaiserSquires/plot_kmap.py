16fa2ac37c81e2bb9d0c8a3227d70df82f76762b
GeorgeVassilakis <vassilakis.g@northeastern.edu>
2024-02-07 20:01:50 -0500

    Fix axes labelling issue and match RA/DEC origin convention

--- SMPy/KaiserSquires/plot_kmap.py
+++ SMPy/KaiserSquires/plot_kmap.py
@@ -5,16 +5,16 @@ def plot_convergence(convergence, boundaries, config):
     # Apply Gaussian filter 
     filtered_convergence = gaussian_filter(convergence, config['gaussian_kernel'])
 
-    # Plot the convergence map
     plt.figure(figsize=config['figsize'])
     plt.imshow(filtered_convergence[:, ::-1], 
                cmap=config['cmap'],
                vmax=config['vmax'], 
                vmin=config['vmin'],
-               extent=[boundaries['ra_min'], 
-                       boundaries['ra_max'], 
+               extent=[boundaries['ra_max'], 
+                       boundaries['ra_min'], 
                        boundaries['dec_min'], 
-                       boundaries['dec_max']])
+                       boundaries['dec_max']],
+               origin='lower')  # Sets the origin to bottom left to match the RA/DEC convention
     plt.colorbar()
     plt.xlabel(config['xlabel'])
     plt.ylabel(config['ylabel'])
