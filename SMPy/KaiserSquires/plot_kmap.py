1e610a863222a931d2a2552652b9408943068b8e
data_vadar <rocks.sayan03@gmail.com>
2024-10-30 11:34:58 -0400

    added a new load shear function

--- SMPy/KaiserSquires/plot_kmap.py
+++ SMPy/KaiserSquires/plot_kmap.py
@@ -195,6 +195,105 @@ def plot_convergence_v2(convergence, boundaries, config, output_name="Converenge
     fig.savefig(output_name)
     print(f"Convergence map saved as PNG file: {output_name}")
     plt.close(fig)
+    
+def plot_convergence_v3(convergence, boundaries, config, output_name="Converenge map", center_cl=None, smoothing=None, vmax=None, vmin=None, title=None, threshold = None):
+    """
+    Make plot of convergence map and save to file using information passed
+    in run configuration file. 
+
+    Arguments
+        convergence: XXX raw convergence map XXX
+        boundaries: XXX RA/Dec axis limits for plot, set in XXX
+        config: overall run configuration file
+
+    """
+
+    # Embiggen font sizes, tick marks, etc.
+    fontsize = 15
+    plt.rcParams.update({'axes.linewidth': 1.3})
+    plt.rcParams.update({'xtick.labelsize': fontsize})
+    plt.rcParams.update({'ytick.labelsize': fontsize})
+    plt.rcParams.update({'xtick.major.size': 8})
+    plt.rcParams.update({'xtick.major.width': 1.3})
+    plt.rcParams.update({'xtick.minor.visible': True})
+    plt.rcParams.update({'xtick.minor.width': 1.})
+    plt.rcParams.update({'xtick.minor.size': 6})
+    plt.rcParams.update({'xtick.direction': 'in'})
+    plt.rcParams.update({'ytick.major.width': 1.3})
+    plt.rcParams.update({'ytick.major.size': 8})
+    plt.rcParams.update({'ytick.minor.visible': True})
+    plt.rcParams.update({'ytick.minor.width': 1.})
+    plt.rcParams.update({'ytick.minor.size':6})
+    plt.rcParams.update({'ytick.direction':'in'})
+    plt.rcParams.update({'axes.labelsize': fontsize})
+    plt.rcParams.update({'axes.titlesize': fontsize})
+
+    
+    # Apply Gaussian filter -- is this the right place to do it?
+    # We are planning on implementing other filters at some point, right?
+    #filtered_convergence = gaussian_filter(convergence, config['gaussian_kernel'])
+    
+    if smoothing is not None:
+        filtered_convergence = gaussian_filter(convergence, smoothing)
+    else:
+        filtered_convergence = convergence
+        
+    if threshold is not None:
+        y, x, h = find_peaks2d(filtered_convergence, threshold=threshold, include_border=False)
+    else: # empty lists
+        x, y, h = [], [], []
+
+    # Make the plot!
+    fig, ax = plt.subplots(
+        nrows=1, ncols=1, figsize=config['figsize'], tight_layout=True
+    )
+
+    
+    im = ax.imshow(
+        filtered_convergence[:, ::-1], 
+        cmap=config['cmap'],
+        vmax=vmax, 
+        vmin=vmin,
+        extent=[boundaries['ra_max'], 
+                    boundaries['ra_min'], 
+                    boundaries['dec_min'], 
+                    boundaries['dec_max']],
+        origin='lower' # Sets the origin to bottom left to match the RA/DEC convention
+    )
+    
+    if center_cl is not None:
+        ra_c, dec_c = center_cl["ra_c"], center_cl["dec_c"]
+        ax.plot(ra_c, dec_c, 'wx', markersize=10)
+        
+    # convert x,y to ra,dec
+    ra_peak, dec_peak = [], []
+    for i in range(len(x)):
+        ra_peak.append(boundaries['ra_min'] + (x[i]+0.5) * (boundaries['ra_max'] - boundaries['ra_min']) / filtered_convergence.shape[1])
+        dec_peak.append(boundaries['dec_min'] + (y[i]+0.5) * (boundaries['dec_max'] - boundaries['dec_min']) / filtered_convergence.shape[0])
+    
+    ax.scatter(ra_peak, dec_peak, s=100, facecolors='none', edgecolors='g', linewidth=1.5)
+      
+    ax.set_xlabel(config['xlabel'])
+    ax.set_ylabel(config['ylabel'])
+    ax.set_title(title)
+
+    # Is there a better way to force something to be a boolean?
+    if config['gridlines'] == True:
+        ax.grid(color='black')
+
+    # Add colorbar; turn off minor axes first
+    plt.rcParams.update({'ytick.minor.visible': False})
+    plt.rcParams.update({'xtick.minor.visible': False})
+
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.07)
+    fig.colorbar(im, cax=cax)
+
+    # Save to file and exit, redoing tight_layout b/c sometimes figure gets cut off 
+    fig.tight_layout() 
+    plt.show()
+    #fig.savefig(config['output_path'])
+    plt.close(fig)
 
 def plot_animation(convergence, boundaries, config, output_name='animation.mp4', center_cl=None, smoothing=False, num_frames=50, fps=5):
     """
